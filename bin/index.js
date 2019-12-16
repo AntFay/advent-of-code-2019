c9522e83bb9d6e1e4e4a2be7c68ce1f96c878060
AntFay <thor94amf@gmail.com>
2019-12-15 23:41:11 -0600

    Day 6 Solution

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,7 @@ var day2 = require('./solutions/day2.js');
 var day3 = require('./solutions/day3.js');
 var day4 = require('./solutions/day4.js');
 var day5 = require('./solutions/day5.js');
-// var day6 = require('./solutions/day6.js');
+var day6 = require('./solutions/day6.js');
 // var day7 = require('./solutions/day7.js');
 // var day8 = require('./solutions/day8.js');
 // var day9 = require('./solutions/day9.js');
@@ -27,79 +27,79 @@ var day5 = require('./solutions/day5.js');
 // var day25 = require('./solutions/day25.js');
 
 console.log("Advent of Code 2019");
-console.log("Tony Fay's Solutions:\n");
+console.log("Tony Fay's Solutions:");
 
-console.log("DAY 1");
+console.log("\nDAY 1");
 console.log(day1.solve());
 
-console.log("DAY 2");
+console.log("\nDAY 2");
 console.log(day2.solve());
 
-console.log("DAY 3");
+console.log("\nDAY 3");
 console.log(day3.solve());
 
-console.log("DAY 4");
+console.log("\nDAY 4");
 console.log(day4.solve());
 
-console.log("DAY 5");
+console.log("\nDAY 5");
 console.log(day5.solve());
 
-// console.log("DAY 6");
-// console.log(day6.solve());
-//
-// console.log("DAY 7");
+console.log("\nDAY 6");
+console.log(day6.solve());
+
+// console.log("\nDAY 7");
 // console.log(day7.solve());
 //
-// console.log("DAY 8");
+// console.log("\nDAY 8");
 // console.log(day8.solve());
 //
-// console.log("DAY 9");
+// console.log("\nDAY 9");
 // console.log(day9.solve());
 //
-// console.log("DAY 10");
+// console.log("\nDAY 10");
 // console.log(day10.solve());
 //
-// console.log("DAY 11");
+// console.log("\nDAY 11");
 // console.log(day11.solve());
 //
-// console.log("DAY 12");
+// console.log("\nDAY 12");
 // console.log(day12.solve());
 //
-// console.log("DAY 13");
+// console.log("\nDAY 13");
 // console.log(day13.solve());
 //
-// console.log("DAY 14");
+// console.log("\nDAY 14");
 // console.log(day14.solve());
 //
-// console.log("DAY 15");
+// console.log("\nDAY 15");
 // console.log(day15.solve());
 //
-// console.log("DAY 16");
+// console.log("\nDAY 16");
 // console.log(day16.solve());
 //
-// console.log("DAY 17");
+// console.log("\nDAY 17");
 // console.log(day17.solve());
 //
-// console.log("DAY 18");
+// console.log("\nDAY 18");
 // console.log(day18.solve());
 //
-// console.log("DAY 19");
+// console.log("\nDAY 19");
 // console.log(day19.solve());
 //
-// console.log("DAY 20");
+// console.log("\nDAY 20");
 // console.log(day20.solve());
 //
-// console.log("DAY 21");
+// console.log("\nDAY 21");
 // console.log(day21.solve());
 //
-// console.log("DAY 22");
+// console.log("\nDAY 22");
 // console.log(day22.solve());
 //
-// console.log("DAY 23");
+// console.log("\nDAY 23");
 // console.log(day23.solve());
 //
-// console.log("DAY 24");
+// console.log("\nDAY 24");
 // console.log(day24.solve());
 //
-// console.log("DAY 25");
+// console.log("\nDAY 25");
 // console.log(day25.solve());
